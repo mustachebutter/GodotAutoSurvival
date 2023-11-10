99421ea0720e8df112cea7acbad0dcff029362ed
Buttters <duylk99@gmail.com>
2023-11-10 15:41:06 -0500

    Added player node and simple movement

---
+++ rust/src/player.rs
@@ -0,0 +1,48 @@
+use std::str::FromStr;
+use godot::prelude::*;
+
+#[derive(GodotClass)]
+#[class(base=Node2D)]
+struct Player 
+{
+    #[base]
+    node2d: Base<Node2D>
+}
+
+use godot::{engine::Node2DVirtual, prelude::Node2D};
+
+#[godot_api]
+impl Node2DVirtual for Player 
+{
+    fn init(node2d: Base<Node2D>) -> Self 
+    {
+        godot_print!("Created Player node!");
+    Self { node2d }
+    }
+
+    fn process(&mut self, delta: f64) 
+    {
+        let input = Input::singleton();
+        let mut velocity = Vector2::new(0.0, 0.0);
+
+        if Input::is_action_pressed(&input, StringName::from_str("Up").unwrap())
+        {
+            velocity.y -= 1.0;
+        }
+        if Input::is_action_pressed(&input, StringName::from_str("Left").unwrap())
+        {
+            velocity.x -= 1.0;
+        }
+        if Input::is_action_pressed(&input, StringName::from_str("Down").unwrap())
+        {
+            velocity.y += 1.0;
+        }
+        if Input::is_action_pressed(&input, StringName::from_str("Right").unwrap())
+        {
+            velocity.x += 1.0;
+        }
+
+        let mut pos = self.node2d.get_position();
+        self.node2d.set_position(pos + velocity)
+    }
+}
